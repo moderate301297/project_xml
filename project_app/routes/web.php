342ceb857d598c2192f2ae77ba5e26cdd119a748
= <=>
2019-05-18 17:20:15 +0700

    luong

--- project_app/routes/web.php
+++ project_app/routes/web.php
@@ -12,7 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Route::get('get-information', '\App\Http\Controllers\HomeController@index');
+
+Route::get('home',function(){
+	return view('index');
+})
